b068c881c4e4405b16507868916da3f2342c20d9
gonzaloguiher <zalo.agui1@gmail.com>
2020-01-12 20:43:27 +0100

    [New] - Funcionalidad traducción al español  ----------------------------------- | Se ha implementado la traducción  | | al español del proyecto           |  ----------------------------------- [Traducción]
    Tipo -> New, Update, Fix, Docs, Test, Merge, Style
    
    	modificado:     decide/decide/settings.py
    	nuevo archivo:  decide/locale/en_US/LC_MESSAGES/django.po
    	nuevo archivo:  decide/locale/es/LC_MESSAGES/django.po

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 
+from django.utils.translation import ugettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -91,6 +93,7 @@ APIS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -155,6 +158,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGES = [
+    ('es', _('Español')),
+    ('en-us', _('English'))
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 LANGUAGE_CODE = 'en-us'
 
