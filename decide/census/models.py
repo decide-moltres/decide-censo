f6b8d2e6ac34e23d4f9a21706125252fd6287969
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-01-24 19:30:39 +0100

    Update - Actualización del modelo de censo -------------------- Se ha añadido el atributo name para identificar mejor el censo para los usuarios. También se ha cambiado el atributo voting_id para poder seleccionar la votación en vez de la id.				 | -------------------- [master]

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from voting.models import Voting
 
 class Census(models.Model):
-    voting_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=50)
+    voting_id = models.ForeignKey(Voting,on_delete=models.CASCADE)
     voter_id = models.ManyToManyField(User)
 
     class Meta:
-        unique_together = (('voting_id'),)
+        unique_together = (('name'),('voting_id'),)
 
     @classmethod
     def get_by_id(cls, cid):
